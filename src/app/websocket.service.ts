e4ffbdc1246357da00ba8602a7d416756188df1e
Esardini <emanuele.sardini@artexe.com>
2018-10-30 16:33:16 +0100

    phone page bootstrap 4

--- src/app/websocket.service.ts
+++ src/app/websocket.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import * as Rx from 'rxjs/Rx';
 import { environment } from '../environments/environment';
 import { socketmessage } from './classes/socketMessage';
